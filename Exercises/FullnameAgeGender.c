e2e44ef0e0cefb5d752ed00659158a9440619aef
Urban Fila <90254842+UrbanFila@users.noreply.github.com>
2022-02-08 23:01:13 +0100

    Create FullnameAgeGender.c

---
+++ Exercises/FullnameAgeGender.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+
+    /*printf("Full name: Naruto Uzumaki\n");
+    printf("Age: 32\n");
+    printf("Gender: male\n");*/
+
+    printf("Full name: Naruto Uzumaki\nAge: 32\nGender: male\n");
+
+    return 0;
+}
