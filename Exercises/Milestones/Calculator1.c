8cacadcb32efff7a85646ce6acf31ae4eb5bdf9c
Urban Fila <90254842+UrbanFila@users.noreply.github.com>
2022-02-09 14:24:22 +0100

    Create Calculator1.c

---
+++ Exercises/Milestones/Calculator1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+
+int main(){
+
+    printf("%d + %d = %d\n", 5, 5, 5+5);
+
+    printf("%d - %d = %d\n", 9, 4, 9-4);
+
+    printf("%d * %d = %d\n", 25, 4, 25*4);
+
+    printf("%d / %d = %d\n", 8, 2, 8/2);
+
+    printf("%d %% %d = %d\n",10, 3, 10%3);
+
+    return 0;
+}
