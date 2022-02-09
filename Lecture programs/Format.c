2c00b34810ee7b1504ccf42f1385b07838509efb
Urban Fila <90254842+UrbanFila@users.noreply.github.com>
2022-02-09 14:16:30 +0100

    Create Format.c

---
+++ Lecture programs/Format.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+
+    printf("Today I am %d old and next year I will be %d old.\n", 20, 21);
+
+    printf("My avarage grade is: %.1f\n", 93.7);
+
+    return 0;
+}
