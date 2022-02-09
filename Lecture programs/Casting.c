446e1f7582a767ab4c1666091406cbef437c1a6a
Urban Fila <90254842+UrbanFila@users.noreply.github.com>
2022-02-09 16:58:37 +0100

    Add files via upload

---
+++ Lecture programs/Casting.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+
+    int num1 = 5;
+    int num2 = 2.0;
+    double result = 0;
+
+    result = ((double) num1) / num2;
+
+    printf("result is %lf\n", result);
+
+
+    return 0;
+}
