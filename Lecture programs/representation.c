cbf6dd8ed947f06bc832a707acb6e449061a7ea0
Urban Fila <90254842+UrbanFila@users.noreply.github.com>
2022-03-08 21:24:24 +0100

    Add files via upload

---
+++ Lecture programs/representation.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+
+
+    char myChar1 = 'a';
+
+    printf("Character: %c \n", myChar1);
+    printf("Decimal ASCII: %d \n", myChar1);
+    printf("Hexadecimal ASCII Code: %X \n", myChar1);
+
+
+
+    return 0;
+}
