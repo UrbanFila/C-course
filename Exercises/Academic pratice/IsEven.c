6e5e4dfd9a1dc4ae3f254a95abe0f00dc274cea0
Urban Fila <90254842+UrbanFila@users.noreply.github.com>
2022-04-04 20:01:32 +0200

    Add files via upload

---
+++ Exercises/Academic pratice/IsEven.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int IsEven(int n){
+
+    if(n % 2 == 0){
+        return 1;
+    }
+    else{
+        return 0;
+    }
+}
+
+int  main(){
+
+    int number;
+
+    printf("Enter number: \n");
+
+    scanf("%d", &number);
+
+
+    int even = IsEven(number);
+
+    if(even == 1){
+        printf("Number %d is even\n", number);
+    }
+    else{
+        printf("Number %d is odd\n", number);
+    }
+
+    return 0;
+}
